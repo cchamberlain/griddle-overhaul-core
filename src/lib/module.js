23f674bddb4c0831c90e506ee5e587bb862d17df
Cole Chamberlain <cole.chamberlain@gmail.com>
2016-04-14 17:02:51 -0700

    moving things to library related libs

--- src/lib/module.js
+++ src/lib/module.js
@@ -1,11 +1,11 @@
-import * as reducers from './reducers';
-import * as states from './initialStates/index';
-import GriddleReducer from './reducers/griddle-overhaul-reducer';
-import * as GriddleActions from './actions/local-actions';
-import * as GriddleHelpers from './helpers';
+import * as reducers from './reducers'
+import * as states from './initialStates/index'
+import GriddleReducer from './reducers/griddle-overhaul-reducer'
+import * as GriddleActions from './actions/local-actions'
+import * as GriddleHelpers from './helpers'
 
 export { reducers as Reducers }
 export { states as States }
-export { GriddleActions as GriddleActions };
-export { GriddleReducer as GriddleReducer };
+export { GriddleActions as GriddleActions }
+export { GriddleReducer as GriddleReducer }
 export {GriddleHelpers as GriddleHelpers}
